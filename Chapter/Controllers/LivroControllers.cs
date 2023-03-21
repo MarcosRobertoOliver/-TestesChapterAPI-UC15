77109f82b36cbc7db804a47155d7e8a7e921715a
Marcos Roberto de Oliveira <marcosroberto@tce.ms>
2023-03-21 08:25:31 -0400

    TestesAPI-UC15

---
+++ Chapter/Controllers/LivroControllers.cs
@@ -0,0 +1,38 @@
+﻿using Chapter.Models;
+using Chapter.Repositores;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroControllers : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+        public LivroControllers(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return ok(_livroRepository.Ler());
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        
+       
+        }
+
+        private IActionResult ok(List<Livro> livros)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Chapter/Models/Livro.cs
@@ -0,0 +1,10 @@
+﻿namespace Chapter.Models
+{
+    public class Livro
+    {
+        public int id { get; set; }
+        public string? Titulo { get; set; }
+        public int QuantidadePaginas { get; set; }
+        public bool Disponivel { get; set; }
+    }
+}
